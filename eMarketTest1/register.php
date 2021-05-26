673002fabdf7dd6f65148b6bd1b2bd1cd958029a
SiamSarker <sarkersiam2@gmail.com>
2021-05-26 11:41:06 +0600

    register page

---
+++ eMarketTest1/register.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Register</title>
+    </head>
+    
+    <body>
+        <h4>Register</h4>
+        <form action="registeruser.php" method="POST">
+            <label for="usrname">Username</label>:
+            <input type="text" id="usrname" name="usrname">
+            <br>
+            <label for="mypass">Password</label>:
+            <input type="password" id="mypass" name="mypass">
+            <br>
+            <label for="myname">Email</label>:
+            <input type="text" id="myname" name="myname">
+            <br>
+            <label for="addrees">Address</label>:
+            <input type="text" id="address" name="address">
+            <br>
+            <label for="contact">Contact No</label>:
+            <input type="number" id="contact" name="contact">
+            <br>
+            <label for="account">Account No</label>:
+            <input type="number" id="account" name="account">
+            <br>
+            <label for="district">District</label>:
+            <input type="text" id="district" name="district">
+            <br>
+            <label for="city">City</label>:
+            <input type="text" id="city" name="city">
+            <br>
+            <br>
+            <input type="submit" value="Click to Register">
+            
+        </form>
+    </body>
+</html>
