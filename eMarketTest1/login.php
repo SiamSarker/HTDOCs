8131a913448e30aac585b026695b09f3aee42fb4
SiamSarker <sarkersiam2@gmail.com>
2021-05-28 09:37:45 +0600

    log in

--- eMarketTest1/login.php
+++ eMarketTest1/login.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 
 <html lang="en">
@@ -16,8 +20,32 @@
         <label for="mypass">Password</label>:
         <input type="password" id="mypass" name="mypass">
         <br>
+        <div>
+			<input type="radio" name="Role" value="farmer"> Farmer<br>
+			<input type="radio" name="Role" value="buyer"> Buyer<br>
+		</div>
         <input type="submit" value="Click to Login">
     </form>
+
+    <div class="textbox"><h3>Don't have an account? Sign up!</h3></div>
+
+				<h1>Select user type and click sign up</h1>
+
+    <form action="register.php" method="POST"> 
+        <div>
+			  <input type="radio" name="Role" value="Farmer"> Sign up as a Farmer<br>
+			  <input type="radio" name="Role" value="Buyer"> Sign up as an Buyer<br>				  
+
+		</div>
+			
+            <button type="submit" class="btn btn-primary">SIGN UP</button>
+
+    </form>
+
+    <?php
+        session_destroy();    
+    ?>
+
 </body>
 
 </html>
